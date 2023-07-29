0980dfd2430190c3430c003ea2b2f802eeec554a
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-07-29 19:49:48 -0300

    feat-editor: Feat - Add configuration's code editor in 'code-editor' component

--- codecamp-front/src/app/shared/components/code-editor/code-editor.component.ts
+++ codecamp-front/src/app/shared/components/code-editor/code-editor.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import { EditorView, basicSetup } from 'codemirror';
+import { javascriptLanguage } from '@codemirror/lang-javascript'
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss']
 })
-export class CodeEditorComponent {
+export class CodeEditorComponent implements AfterViewInit {
+  @ViewChild('ideContainer')
+  ideContainer!: ElementRef<HTMLDivElement>;
 
+  editor!: EditorView;
+
+  ngAfterViewInit(): void {
+    this.editor = new EditorView({
+      parent: this.ideContainer.nativeElement,
+      extensions: [
+        basicSetup,
+        javascriptLanguage
+      ],
+      doc: `console.log('Hello world')`
+    })
+  }
+
+  runCode() {
+    const document = this.editor.state.doc;
+    const code = document.toString()
+
+    eval(code)
+  }
 }
