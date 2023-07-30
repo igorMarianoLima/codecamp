376cdf1b4df8f1453852f512657f31cd77c041e2
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-07-29 22:40:35 -0300

    feat-editor: Feat - Create 'CodeLogger' class to save console logs

---
+++ codecamp-front/src/app/shared/classes/CodeLogger.class.ts
@@ -0,0 +1,30 @@
+interface CodeLog {
+    type: 'message' | 'error';
+    value: any;
+}
+
+export class CodeLogger {
+    logs: CodeLog[];
+
+    constructor() {
+        this.logs = [];
+
+        const logger = console.log.bind(console);
+        console.log = (message: any) => {
+            this.logs.push({
+                type: 'message',
+                value: message
+            });
+            logger(message);
+        }
+
+        const error = console.error.bind(console);
+        console.error = (message: any) => {
+            this.logs.push({
+                type: 'error',
+                value: message
+            });
+            error(message);
+        }
+    }
+}
